ab522962025f018ca71a0a330eaca7cc7e484be9
Myxogastria <50288647+Myxogastria@users.noreply.github.com>
2020-04-20 02:09:21 +0900

    use node but end up with segmentation fault

--- mycc.c
+++ mycc.c
@@ -236,31 +236,18 @@ int main(int argc, char **argv){
 
     user_input = argv[1];
 
-    // トークナイズする
+    // トークナイズ&パース
     token = tokenize(user_input);
+    Node *node = expr();
 
     // アセンブリの前半部分を出力
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
 
-    // 式の最初は数でなければならないので，それをチェックして
-    // 最初のmov命令を出力
-    printf("  mov rax, %d\n", expect_number());
+    gen(node);
 
-    // `+<数>`あるいは`-<数>`というトークンの並びを消費しつつ
-    // アセンブリを出力
-
-    while(!at_eof()){
-        if(consume('+')){
-            printf("  add rax, %d\n", expect_number());
-            continue;
-        }
-
-        expect('-');
-        printf(" sub rax, %d\n", expect_number());
-    }
-
+    printf("  pop rax\n");
     printf("  ret\n");
     return 0;
 }
